59dbdc5267d118eb91c4c010730576e62db44891
gurungalien2727 <gurungalien2727@gmail.com>
2021-01-25 17:11:27 -0600

    resume changed

--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -44,7 +44,7 @@ const About = () => {
                     </a>
                   </div>
                   <div>
-                    <a href="https://drive.google.com/file/d/1kHTJhMMvqx_Nw6z_hNjpOVCzHrGyypFG/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                    <a href="https://drive.google.com/file/d/1CiTeGOPSmtjWQUg8Fhx_fTwnPj0JqS5I/view?usp=sharing" target="_blank" rel="noopener noreferrer">
                       <Button className="m-2" variant="outline-success">
                         My Resume
                       </Button>
